2c319dcf5c2a745b57e19c544365a45b1af0b66f
Erick Hjort <erick.hjort@icloud.com>
2023-06-12 16:34:40 -0600

    selectionSort

--- selection.js
+++ selection.js
@@ -1,3 +1,22 @@
-function selectionSort() {}
+function selectionSort(arr){
+  
+
+    for (i=0; i < arr.length; i++){    
+      
+      let minVal = arr[i];
+      let minPosition = i
+      
+      for (j=i+1; j < arr.length; j++){
+        if (arr[j] < minVal){
+          minVal = arr[j];
+          minPosition = j
+          }
+      }
+      arr[minPosition] = arr[i];
+      arr[i] = minVal;
+    }
+    return arr;
+    
+  }
 
 module.exports = selectionSort;
